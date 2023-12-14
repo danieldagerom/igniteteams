d6d4b34570f37e2e6fb5d1c2776cda17908f47bf
danieldagerom <danielprimo9@gmail.com>
2023-12-14 15:14:10 -0300

    adiciona styled components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Title } from './styles';
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Olá reaact native</Text>
-    </View>
+    <Container>
+      <Title>Olá react native</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
